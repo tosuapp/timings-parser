ddc74fb47140670a842150e984105a1d1b9d0779
ck <21735205+cyperdark@users.noreply.github.com>
2024-07-15 11:04:17 +0300

    multi parse

--- index.js
+++ index.js
@@ -1,53 +1,61 @@
 const fs = require('fs');
+const path = require('path');
 
 
 const graphs = {};
 
-const fileName = 'timings';
-const file_path = `./${fileName}.txt`;
-const output_path = `./${fileName}.json`;
+const fileNames = ['cpp', 'rust'];
+const files_path = `./`;
+const outputs_path = `./`;
 
 
 function main() {
     try {
-        const lines = fs.readFileSync(file_path, 'utf8').split('\n');
-        for (let i = 0; i < lines.length; i++) {
-            if (lines[i].trim() == '') continue;
+        for (let f = 0; f < fileNames.length; f++) {
+            const file_name = fileNames[f];
+            const file_path = path.join(files_path, `${file_name}.txt`);
+            const output_path = path.join(outputs_path, `${file_name}.json`);
+
+            const lines = fs.readFileSync(file_path, 'utf8').split('\n');
+            for (let i = 0; i < lines.length; i++) {
+                if (lines[i].trim() == '') continue;
 
 
-            const line = lines[i].split(',');
-            const name = line[1];
+                const line = lines[i].split(',');
+                const name = line[1];
 
-            const time = +line[0] || new Date(+line[0]);
-            const savingTime = line[2];
-
-
-            const graph = graphs[name] || { series: [], xaxis: { type: "datetime", categories: [] } };
-            const timings = line[3].split('|');
-            for (let t = 0; t < timings.length; t++) {
-                const [key, value] = timings[t].split(':');
+                const time = +line[0] || new Date(+line[0]);
+                const savingTime = line[2];
 
 
-                const find = graph.series.find(r => r.name.toLowerCase() == key.toLowerCase());
-                if (find) {
-                    find.data.push(value);
-                    continue;
+                const graph = graphs[name] || { series: [], xaxis: { type: "datetime", categories: [] } };
+                const timings = line[3].split('|');
+                for (let t = 0; t < timings.length; t++) {
+                    const [key, value] = timings[t].split(':');
+
+
+                    const find = graph.series.find(r => r.name.toLowerCase() == key.toLowerCase());
+                    if (find) {
+                        find.data.push(value);
+                        continue;
+                    };
+
+
+
+                    graph.series.push({
+                        name: key,
+                        data: [value]
+                    });
                 };
 
 
-                graph.series.push({
-                    name: key,
-                    data: [value]
-                });
+                graph.xaxis.categories.push(time);
+                graphs[name] = graph;
             };
 
 
-            graph.xaxis.categories.push(time);
-            graphs[name] = graph;
+            fs.writeFileSync(output_path, JSON.stringify(graphs), 'utf8');
         };
-
-
-        fs.writeFileSync(output_path, JSON.stringify(graphs), 'utf8');
     } catch (error) {
         console.log(error);
     };
